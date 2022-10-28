d06597a14456c4093263ddcb91781be196f93afc
nada adel <engnada.adel99@gmail.com>
2022-10-28 14:59:49 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express'); //import express package
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express(); //app using express
+app.get('/', (req, res) => { // route for display hello world
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log('Application started successfully.');
+});
